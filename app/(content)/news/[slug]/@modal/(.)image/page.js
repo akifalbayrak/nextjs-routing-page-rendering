e995e2abb6dae6dfe5d3223fb68e401f192b5fd7
Akif Albayrak <akifalbayrak6161@gmail.com>
2024-05-02 15:06:20 +0300

    first commit

---
+++ app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -0,0 +1,31 @@
+"use client";
+
+import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
+import { useRouter } from "next/router";
+
+export default function InterceptImagePage({ params }) {
+    const router = useRouter();
+
+    const newItemSlug = params.slug;
+    const newsItem = DUMMY_NEWS.find(
+        (newsItem) => newsItem.slug === newItemSlug
+    );
+    if (!newsItem) {
+        notFound();
+    }
+
+    return (
+        <>
+            <div className="modal-backdrop" onClick={router.back} />
+            <dialog className="modal" open>
+                <div className="fullscreen-image">
+                    <img
+                        src={`/images/news/${newsItem.image}`}
+                        alt={newsItem.title}
+                    />
+                </div>
+            </dialog>
+        </>
+    );
+}
