f1c253426449897d6aea78956bdee8b9a3dfdcdb
Akif Albayrak <akifalbayrak6161@gmail.com>
2024-05-03 14:05:22 +0300

    adding readme and fixing navlink error

--- components/nav-link.js
+++ components/nav-link.js
@@ -8,7 +8,7 @@ export default function NavLink({ href, children }) {
     return (
         <Link
             href={href}
-            className={path.startsWith({ href }) ? "active" : undefined}>
+            className={path.startsWith(href) ? "active" : undefined}>
             {children}
         </Link>
     );
