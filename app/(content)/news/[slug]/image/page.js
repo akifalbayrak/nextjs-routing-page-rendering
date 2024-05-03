d02b4ccc31664ba7f9684ee0c0a3c25dc9f766c9
Akif Albayrak <akifalbayrak6161@gmail.com>
2024-05-03 13:45:10 +0300

    fix(modal): adding backdrop

--- app/(content)/news/[slug]/image/page.js
+++ app/(content)/news/[slug]/image/page.js
@@ -1,3 +1,4 @@
+import ModalBackDrop from "@/components/modal-backdrop";
 import { getNewsItem } from "@/lib/news";
 import { notFound } from "next/navigation";
 
@@ -9,8 +10,14 @@ export default async function ImagePage({ params }) {
     }
 
     return (
-        <div className="fullscreen-image">
-            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-        </div>
+        <>
+            <ModalBackDrop />
+            <div className="fullscreen-image">
+                <img
+                    src={`/images/news/${newsItem.image}`}
+                    alt={newsItem.title}
+                />
+            </div>
+        </>
     );
 }
